4935f908d3912d4df3e2ea49c3f7229fbe508502
ZaguiMaster <github.correo@gmail.com>
2024-12-06 11:38:59 -0500

    Se agregó el metodo clickOnShoppingCartIcon, el cual se encarga de dar click sobre el icono del carrito de compras

--- tests/actions/ProductPageActions.ts
+++ tests/actions/ProductPageActions.ts
@@ -1,11 +1,13 @@
 import { Page } from "@playwright/test";
 import { ProductPage } from "../pages/ProductPage";
+import { HeaderPage } from "../pages/HeaderPage"
 
 
 export class ProductPageActions {
 
   readonly page: Page;
   readonly productPage: ProductPage
+  readonly headerPage: HeaderPage
   productName: string
   productDescription: string
   productPrice: string
@@ -14,6 +16,7 @@ export class ProductPageActions {
   constructor(page: Page) {
     this.page = page
     this.productPage = new ProductPage(page)
+    this.headerPage = new HeaderPage(page)
   }
 
   private async getProducts() {
@@ -34,7 +37,10 @@ export class ProductPageActions {
     this.productDescription = await product.locator(this.productPage.productDescription).innerText()
     this.productPrice = await product.locator(this.productPage.productPrice).innerText()
     await product.locator(this.productPage.addToCartButton).click()
+  }
 
+  async clickOnShoppingCartIcon() {
+    await this.headerPage.shoppingCartIcon.click()
   }
 
 }
