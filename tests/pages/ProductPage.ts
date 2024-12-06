0fd02c0924e58c968a4e90884f659c8e5a42048e
ZaguiMaster <github.correo@gmail.com>
2024-12-06 08:35:59 -0500

    Se agregaron nuevos localizadores a la clase

--- tests/pages/ProductPage.ts
+++ tests/pages/ProductPage.ts
@@ -3,14 +3,22 @@ import { Page, Locator } from "@playwright/test"
 export class ProductPage {
 
   readonly page: Page
-  readonly titleProducts: Locator
+  readonly titlePageProducts: Locator
   readonly selectFilter: Locator
   readonly cardProduct: Locator
+  readonly productName: Locator
+  readonly productDescription: Locator
+  readonly productPrice: Locator
+  readonly addToCartButton: Locator
 
   constructor(page: Page) {
     this.page = page
-    this.titleProducts = page.locator('div[data-test="title"]')
+    this.titlePageProducts = page.locator('div[data-test="title"]')
     this.selectFilter = page.locator('select[data-test= "product-sort-container"]')
     this.cardProduct = page.locator('div[data-test="inventory-item"]')
+    this.productName = page.locator('div[data-test="inventory-item-name"]')
+    this.productDescription = page.locator('div[data-test="inventory-item-desc"]')
+    this.productPrice = page.locator('div[data-test="inventory-item-price"]')
+    this.addToCartButton = page.getByRole('button', { name: 'Add to cart' })
   }
 }
